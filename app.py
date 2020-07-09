79ef603bc5d4003661a7b0ebdd2674ca1abbbbe1
Gokul <gokulprakash22@gmail.com>
2020-07-09 18:43:38 +0530

    addded image uploader

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',methods = ['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        file = request.files['image']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('index.html')
 
 if __name__=="__main__":
